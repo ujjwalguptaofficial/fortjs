cefe48053bb46c999d868e2d3ee725f9f8488fa1
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2018-12-15 19:31:49 +0530

    added file test

--- test.sh
+++ test.sh
@@ -1,6 +1,15 @@
-# Run random folder test
+#create the build file
 npm run buildpack
+
+# Run random folder test
+
 cd tests/random
 npm install
+ls
+npm run install:build:test
+
+#run file test
+cd..
+cd filetest
 ls
 npm run install:build:test
