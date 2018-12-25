a7502a5a443c18caf2180c254a29bdbcedaa26da
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2018-12-25 09:59:29 +0530

    remove random folder and added general

--- tests/random/code/extra/custom_error_handler.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorHandler } from "fortjs";
-
-export class CustomErrorHandler extends ErrorHandler {
-    async onForbiddenRequest(){
-       return `<h1>We are sorry, but you are not allowed access to this resource.</h1>`
-    }
-}
--- tests/random/code/extra/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './request_logger';
