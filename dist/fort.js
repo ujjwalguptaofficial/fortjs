16b10e392370305ca84e005dae3a60251f1f8f09
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-04-01 11:01:41 +0530

    deploy version 1.5.2

--- dist/fort.js
+++ dist/fort.js
@@ -1,5 +1,5 @@
 /*!
- * @license :fortjs - V1.5.1 - 01/04/2019
+ * @license :fortjs - V1.5.2 - 01/04/2019
  * https://github.com/ujjwalguptaofficial/fortjs
  * Copyright (c) 2019 @Ujjwal Gupta; Licensed MIT
  */
