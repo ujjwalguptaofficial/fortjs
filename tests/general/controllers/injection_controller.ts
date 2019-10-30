24f01d50b3a28535c08784beb52f987f711842cc
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-10-30 09:48:07 +0530

    release 1.8.3

--- tests/general/controllers/injection_controller.ts
+++ tests/general/controllers/injection_controller.ts
@@ -2,6 +2,11 @@ import { Controller, viewResult, Worker, Route, jsonResult, HTTP_METHOD, Default
 
 export class InjectionController extends Controller {
 
+    // do not remove constructor and value
+    constructor(@Assign('Welcome to fort') title){
+        super();
+    }
+
     @Worker()
     async getMultipleStringInjection(@Assign('Welcome to fort') title, @Assign('Hello World') helloWorld, @Assign('ujjwal') firstName, @Assign('Gupta') lastName) {
         return jsonResult({ value: [title, helloWorld, firstName, lastName] });
