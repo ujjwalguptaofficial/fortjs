4fc931b242255b1c70478d0ebd3ca7718bd8ae49
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2023-08-09 21:48:05 +0400

    optimize default worker

--- src/decorators/default_worker.ts
+++ src/decorators/default_worker.ts
@@ -4,16 +4,9 @@ import { worker } from "./worker";
 
 export const defaultWorker = (...allowedMethods: HTTP_METHOD[]): MethodDecorator => {
     return (target: any, methodName: string, descriptor: PropertyDescriptor) => {
-        if (allowedMethods.length === 0) {
-            worker(HTTP_METHOD.Get)(
-                target, methodName, descriptor
-            );
-        }
-        else {
-            worker(...allowedMethods)(
-                target, methodName, descriptor
-            );
-        }
+        worker(...allowedMethods.length === 0 ? [HTTP_METHOD.Get] : allowedMethods)(
+            target, methodName, descriptor
+        );
         route("/")(target, methodName, descriptor);
     };
 };
