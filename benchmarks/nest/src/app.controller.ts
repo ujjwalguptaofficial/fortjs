ce8553587310dd8b1a4307d0c7c6e855561ff5d6
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2023-11-08 13:59:54 +0400

    fix nest project for benchmarks

--- benchmarks/nest/src/app.controller.ts
+++ benchmarks/nest/src/app.controller.ts
@@ -1,9 +1,12 @@
-import { Get, Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-	@Get()
-	root(): string {
-    return 'Hello World!';
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
