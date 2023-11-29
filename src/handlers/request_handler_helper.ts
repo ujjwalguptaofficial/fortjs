d78a8f7e08eba1aeaf09daefa132b2d0e9af5b87
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2023-11-29 23:08:54 +0400

    cache mime type

--- src/handlers/request_handler_helper.ts
+++ src/handlers/request_handler_helper.ts
@@ -7,6 +7,10 @@ import { HttpFormatResult } from "../types";
 import { parse } from "path";
 import { FileHandler } from "./file_handler";
 
+const jsonMimeType = [MIME_TYPE.Json, MIME_TYPE.Xml];
+const textMimeType = [MIME_TYPE.Text, MIME_TYPE.Html, MIME_TYPE.Js,
+MIME_TYPE.Css, MIME_TYPE.Rtf, MIME_TYPE.Csv];
+
 export class RequestHandlerHelper {
     protected componentProps: IComponentProp;
 
@@ -38,17 +42,15 @@ export class RequestHandlerHelper {
         switch (type) {
             case MIME_TYPE.Json:
             case MIME_TYPE.Xml:
-                return [MIME_TYPE.Json, MIME_TYPE.Xml];
+                return jsonMimeType;
             case MIME_TYPE.Html:
             case MIME_TYPE.Css:
             case MIME_TYPE.Csv:
             case MIME_TYPE.Js:
             case MIME_TYPE.Rtf:
             case MIME_TYPE.Text:
-                return [MIME_TYPE.Text, MIME_TYPE.Html, MIME_TYPE.Js,
-                MIME_TYPE.Css, MIME_TYPE.Rtf, MIME_TYPE.Csv];
+                return textMimeType;
         }
-        return null;
     }
 
     protected onBadRequest(error) {
