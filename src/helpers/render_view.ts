f6c2e1ceea4784ada9a75506147e7786f5d720f6
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2024-05-11 14:51:04 +0530

    create executeRenderView and reuse

--- src/helpers/render_view.ts
+++ src/helpers/render_view.ts
@@ -4,22 +4,21 @@ import { Fort } from "../models";
 
 export let renderView: (viewName: string, model?: any) => Promise<string>;
 
+function executeRenderView(viewName: string, model?: any) {
+    return Fort['viewEngine_'].render({
+        view: viewName,
+        model
+    });
+}
+
 if (process.env.NODE_ENV === "production") {
-    renderView = (viewName: string, model?: any) => {
-        return Fort['viewEngine_'].render({
-            view: viewName,
-            model
-        });
-    };
+    renderView = executeRenderView;
 }
 else {
     renderView = (viewName: string, model?: any) => {
         if (Fort['viewEngine_'] == null) {
             new LogHelper(ERROR_TYPE.UndefinedViewEngine).throw();
         }
-        return Fort['viewEngine_'].render({
-            view: viewName,
-            model
-        });
+        return executeRenderView(viewName, model);
     };
 }
