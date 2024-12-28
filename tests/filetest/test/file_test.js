97bb1809df29e94226e9216a25e6290f57b97655
ujjwalgupta94 <ujjwalkumargupta44@gmail.com>
2024-12-28 12:56:59 +0530

    add test for query string check in file upload

--- tests/filetest/test/file_test.js
+++ tests/filetest/test/file_test.js
@@ -75,6 +75,31 @@ describe("/file", () => {
     })
 
     it('/upload with right file name', (done) => {
+        request.post('/file/upload').attach("jsstore", "static/fort_js_logo_200_137.png").end((err, res) => {
+            expect(err).to.be.null;
+            console.log("body", res.body);
+            expect(res).to.have.status(200);
+            expect(res).to.have.header('content-type', 'application/json');
+            expect(res.body.responseText).to.be.equal('file saved');
+            expect(res.body.fieldName).to.be.equal('jsstore');
+            expect(res.body.count).to.be.equal(1);
+            expect(res.body.fileName).to.be.equal('fort_js_logo_200_137.png');
+            expect(res.body.data).to.eql({
+                "fileInfo": {
+                    "fieldName": "jsstore",
+                    "fileName": "fort_js_logo_200_137.png"
+                },
+                "query": {
+                },
+                "singletonFileName": "upload.png",
+                "className": "FileName"
+            });
+            done();
+        });
+    })
+
+
+    it('/upload with right file name with query', (done) => {
         request.post('/file/upload?test=1').attach("jsstore", "static/fort_js_logo_200_137.png").end((err, res) => {
             expect(err).to.be.null;
             console.log("body", res.body);
